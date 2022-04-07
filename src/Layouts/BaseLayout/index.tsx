ac9dccee40b1deb50f95bbb2aba59a05498fef3d
Santos Pierre <santospierre1305@gmail.com>
2022-04-07 15:03:59 +0200

    Markdown Editor

--- src/Layouts/BaseLayout/index.tsx
+++ src/Layouts/BaseLayout/index.tsx
@@ -1,8 +1,28 @@
+import { LogoutIcon } from '@heroicons/react/solid';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 const BaseLayout: React.FC = ({ children }) => {
+    const { pathname } = useLocation();
+
     return (
         <div className="min-h-full text-gray-200 bg-gray-800 font-body">
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <main className="max-w-5xl mx-auto">{children}</main>
+                <main className="max-w-5xl mx-auto">
+                    {children}
+                    {pathname !== '/' && (
+                        <div className="flex justify-center mx-auto my-5">
+                            <Link to={'/'}>
+                                <div className="inline-flex items-center group">
+                                    <LogoutIcon className="w-8 h-8 transition-all duration-500 transform rotate-180 text-react group-hover:text-white" />
+                                    <span className="text-2xl font-bold underline transition-all duration-500 group-hover:text-react">
+                                        Go Back
+                                    </span>
+                                </div>
+                            </Link>
+                        </div>
+                    )}
+                </main>
             </div>
         </div>
     );
--- src/pages/MarkdownEditor.tsx
+++ src/pages/MarkdownEditor.tsx
@@ -2,10 +2,13 @@ import { ChangeEvent, useState } from 'react';
 import PageTitle from '../components/ui/Title/PageTitle';
 import BaseLayout from '../Layouts/BaseLayout';
 import { micromark } from 'micromark';
+import { PencilIcon } from '@heroicons/react/solid';
+import clsx from 'clsx';
 
 const MarkdownEditor = () => {
-    const [content, setContent] = useState<string>('');
-    const [markdown, setMarkdown] = useState<string>('');
+    const [content, setContent] = useState<string>('# Hello This is a markdown editor');
+    const [markdown, setMarkdown] = useState<string>(micromark(content));
+    const [isEditing, setEditing] = useState<boolean>(true);
 
     const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         setContent(event.target.value);
@@ -24,16 +27,47 @@ const MarkdownEditor = () => {
                     </div>
                 </div>
                 <div className="grid grid-cols-[50px,1fr]">
-                    <div className="bg-gray-700" />
-                    <textarea
-                        className="p-2 bg-gray-600 resize-none focus:outline-none"
-                        value={content}
-                        onChange={handleChange}
-                    />
+                    <div className="bg-gray-700">
+                        <div
+                            className={clsx(
+                                { 'bg-gray-600': isEditing },
+                                'inline-flex items-center justify-center w-full py-3 cursor-pointer hover:bg-gray-600'
+                            )}
+                            onClick={() => setEditing(true)}
+                        >
+                            <PencilIcon className="w-8 h-8" />
+                        </div>
+                        <div
+                            className={clsx(
+                                { 'bg-gray-600': !isEditing },
+                                'inline-flex items-center justify-center w-full py-3 cursor-pointer hover:bg-gray-600'
+                            )}
+                            onClick={() => setEditing(false)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                                className="w-8 h-8 fill-current stroke-current"
+                            >
+                                <path d="M475 64H37C16.58 64 0 81.38 0 102.77v306.42C0 430.59 16.58 448 37 448h438c20.38 0 37-17.41 37-38.81V102.77C512 81.38 495.42 64 475 64zM288 368h-64V256l-48 64-48-64v112H64V144h64l48 80 48-80h64zm96 0l-80-112h48.05L352 144h64v112h48z" />
+                            </svg>
+                        </div>
+                    </div>
+                    {isEditing ? (
+                        <textarea
+                            className="p-2 bg-gray-600 resize-none focus:outline-none"
+                            value={content}
+                            onChange={handleChange}
+                        />
+                    ) : (
+                        <section
+                            className="max-h-[40rem] p-2 overflow-y-scroll prose bg-gray-600 prose-invert max-w-none"
+                            dangerouslySetInnerHTML={{ __html: markdown }}
+                        ></section>
+                    )}
                 </div>
                 <div className="rounded-b-md bg-sky-600"></div>
             </div>
-            <div className="prose prose-invert" dangerouslySetInnerHTML={{ __html: markdown }}></div>
         </BaseLayout>
     );
 };
