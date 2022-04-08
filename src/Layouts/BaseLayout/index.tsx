18b64c500172c5c096abd899498a897dfbd2edf5
Santos Pierre <santospierre1305@gmail.com>
2022-04-08 11:52:55 +0200

    Add Footer to go back to homepage

--- src/Layouts/BaseLayout/index.tsx
+++ src/Layouts/BaseLayout/index.tsx
@@ -8,22 +8,22 @@ const BaseLayout: React.FC = ({ children }) => {
     return (
         <div className="min-h-full text-gray-200 bg-gray-800 font-body">
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <main className="max-w-5xl mx-auto">
-                    {children}
-                    {pathname !== '/' && (
-                        <div className="flex justify-center mx-auto my-5">
-                            <Link to={'/'}>
-                                <div className="inline-flex items-center group">
-                                    <LogoutIcon className="w-8 h-8 transition-all duration-500 transform rotate-180 text-react group-hover:text-white" />
-                                    <span className="text-2xl font-bold underline transition-all duration-500 group-hover:text-react">
-                                        Go Back
-                                    </span>
-                                </div>
-                            </Link>
-                        </div>
-                    )}
-                </main>
+                <main className="max-w-5xl mx-auto">{children}</main>
             </div>
+            <footer className="fixed bottom-0 w-full">
+                {pathname !== '/' && (
+                    <div className="flex justify-center mx-auto my-5">
+                        <Link to={'/'}>
+                            <div className="inline-flex items-center group">
+                                <LogoutIcon className="w-8 h-8 transition-all duration-500 transform rotate-180 text-react group-hover:text-white" />
+                                <span className="text-2xl font-bold underline transition-all duration-500 group-hover:text-react">
+                                    Go Back
+                                </span>
+                            </div>
+                        </Link>
+                    </div>
+                )}
+            </footer>
         </div>
     );
 };
